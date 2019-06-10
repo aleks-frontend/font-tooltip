0439d8ba0f0291f37849e5de08a687a25e8d1acf
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-06-10 17:30:40 +0200

    chore: mouseneter and mouseleave events added to JS function - position check feature added (opposite style for when there is not enough room on top of the text element)

--- font-tooltip.js
+++ font-tooltip.js
@@ -1,21 +1,46 @@
 function fontTooltip(showTooltip=false, unit='px') {
   if ( !showTooltip ) return;
   const textBlocks = document.querySelectorAll('.js-font-tooltip');
-  textBlocks.forEach(textBlock => {
-    const style = window.getComputedStyle(textBlock);
-    let fontSize = style.getPropertyValue('font-size');
-    const fontFamily = style.getPropertyValue('font-family');
+
+  function addTooltip() {
+    const computedStyle = window.getComputedStyle(this);
+    const blockProperties = this.getBoundingClientRect();
+    let fontSize = computedStyle.getPropertyValue('font-size');
+    const fontFamily = computedStyle.getPropertyValue('font-family');
     const tooltip = document.createElement('div');
+    const tooltipLeftPos = ( blockProperties.x + blockProperties.width / 2 );
+    let oppositePos;
 
     if ( unit == 'pt' ) fontSize = `${parseFloat(fontSize) * 0.75}pt`;
 
     tooltip.classList.add('tooltip');
     tooltip.innerHTML = `
-Font Family: <strong>${fontFamily}</strong><br />
-Font Size: <strong>${fontSize}</strong>
-`;
+      Font Family: <strong>${fontFamily}</strong><br />
+      Font Size: <strong>${fontSize}</strong>
+    `;
 
-    textBlock.style.position = 'relative';
-    textBlock.appendChild(tooltip);
+    this.style.position = 'relative';
+    document.body.appendChild(tooltip);
+
+    oppositePos = ( blockProperties.y < tooltip.offsetHeight );
+
+    tooltip.style.left = tooltipLeftPos + 'px';
+    if ( oppositePos ) {
+      tooltip.style.top = (blockProperties.y + blockProperties.height + 5) + 'px';
+      tooltip.classList.add('tooltip--opposite');
+    } else {
+      tooltip.style.top = (blockProperties.y - tooltip.offsetHeight - 5) + 'px';
+      tooltip.classList.remove('tooltip--opposite');
+    } 
+  }
+
+  function removeTooltip() {
+    const tooltip = document.querySelector('.tooltip');
+    document.body.removeChild(tooltip);
+  }
+
+  textBlocks.forEach(textBlock => {
+    textBlock.addEventListener('mouseenter', addTooltip);
+    textBlock.addEventListener('mouseleave', removeTooltip);
   });
 }
